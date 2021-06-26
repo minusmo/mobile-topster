d8a1fed75c7a367616b17e2b68a52b9cbfa07ca3
hojooneum <bldolphin96@gmail.com>
2021-06-27 02:48:33 +0900

    [FIX] ul list-style fix (none)

--- src/App.js
+++ src/App.js
@@ -996,6 +996,7 @@ function App() {
       <div style={{ marginTop: '1em' }}>
         <h5>사용방법: desktop 탑스터와 비슷합니다.</h5>
         <h5>SetGrid와 Set42는 반드시 reset후에 설정해주시기 바랍니다.</h5>
+        <h5>Chrome 브라우저에서 정상 작동합니다.</h5>
         <h5>이미지를 클릭하면 새로운 앨범을 <br></br>검색하고 추가할 수 있습니다.</h5>
         <h5>문의: bldolphin96@gmail.com</h5>
       </div>
