d858966db910a66af4d1a285843890f5072ba907
hojooneum <bldolphin96@gmail.com>
2022-04-10 23:24:59 +0900

    [FIX]{gitignore} ignored files updated

--- src/constants/httpConstants.ts
+++
@@ -1,2 +0,0 @@
-export const SPOTIFY_API: string = "https://accounts.spotify.com/api/token";
-export const CONTENT_TYPE: string = "application/x-www-form-urlencoded";
--- src/utils/httpUtils.ts
+++
@@ -1,7 +0,0 @@
-
-export const getAlbumsByArtistandAlbumName = (query: string, country: string): string => {
-  return `https://api.spotify.com/v1/search/?q=album:${query}%20OR%20artist:${query}&type=album&market=${country}&limit=50`;
-}
-export const getAlbumsByAlbumName = (query:string, country:string):string => {
-  return `https://api.spotify.com/v1/search/?q=${query}&type=album&market=${country}&limit=50`;
-}
