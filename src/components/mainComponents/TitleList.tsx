805044c48d2797fa45f7e2fbd74bc5f45fb5b040
hojooneum <bldolphin96@gmail.com>
2022-04-10 12:04:53 +0900

    [UPDATE]{TitleList.tsx} TitleList component updated to tsx

---
+++ src/components/mainComponents/TitleList.tsx
@@ -0,0 +1,46 @@
+import * as _ from "lodash";
+import { Grid } from "../../models/types";
+type TitleListElementProps = {
+  title: String;
+}
+
+type TitleListElementType = JSX.Element | null;
+const TitleListElement = ({ title }: TitleListElementProps): TitleListElementType => {
+  return (title.length !== 0 ? <li>{title}</li> : null);
+}
+
+type TitleListProps = {
+  rows: number;
+  cols: number;
+  showAlbumTitle: boolean;
+  topsterRows: Grid;
+  backgroundColor: string;
+}
+
+const TitleList = ({
+  rows,
+  cols,
+  showAlbumTitle,
+  topsterRows,
+  backgroundColor,
+}: TitleListProps): JSX.Element => {
+  return (
+    <div
+      id="titleList"
+      className={showAlbumTitle ? "titleList-show" : "titleList-hidden"}
+      style={{ backgroundColor: backgroundColor }}
+    >
+      <ul id="titleUnorderedlist">
+        {topsterRows.slice(0, rows).map((row, index) => {
+          return row
+            .slice(0, cols)
+            .map((cell, index) => (
+              <TitleListElement title={cell.alt} key={_.uniqueId()} />
+            ));
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default TitleList;
--- src/models/types.ts
+++ src/models/types.ts
@@ -1,14 +1,12 @@
-type Cell = {
+export type Cell = {
   src: string;
   alt: String;
 };
 
-type Grid = Array<Array<Cell>>;
+export type Grid = Array<Array<Cell>>;
 
-type Titles = {
+export type Titles = {
   _row: Number;
   _col: Number;
-  titleList: Array<Array<Tile>>;
+  titleList: Array<Array<Cell>>;
 };
-
-export { Cell, Grid, Titles };
