9a00815cae898ffe9fd21f5f613b21e898d7d38a
hojooneum <bldolphin96@gmail.com>
2022-11-21 16:35:52 +0900

    refactor: update react root renderer

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import {createRoot} from "react-dom/client";
 import "./styles/index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("div-root"));
+const root = createRoot(document.getElementById("div-root")!);
+root.render(<App/>);
