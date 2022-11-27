00ecf79836312f90fab24794e7679451c38bc3c3
hojooneum <bldolphin96@gmail.com>
2022-11-27 15:28:54 +0900

    Refactor: move utils into TopsterBoard dir

---
+++ src/components/mainComponents/TopsterBoard/utils.ts
@@ -0,0 +1,46 @@
+import { Grid } from "../../../models/modelTypes";
+
+export type TopsterBoardProps = {
+    rows: number;
+    cols: number;
+    topsterType: string;
+    topster: Grid;
+    backgroundColor: string;
+    handleClickGridcell: (e: React.MouseEvent<HTMLDivElement>) => void;
+    isRoundedBorder: boolean;
+    currentWidth: number | undefined;
+  };
+  
+export  type gridContainerStyle = {
+    backgroundColor: string;
+    height?: string;
+    gridBoardRows?: string;
+    gridBoardColumns?: string;
+  };
+  
+export const setGridContainerStyle = (
+    topsterType: string,
+    backgroundColor: string,
+    rows: number,
+    cols: number,
+    currentWidth: number | undefined
+  ): gridContainerStyle => {
+    let gridContainerStyle: gridContainerStyle;
+    if (topsterType === "top42") {
+      gridContainerStyle = {
+        backgroundColor: backgroundColor,
+        gridBoardRows: `repeat(28, calc(${currentWidth}px / 30))`,
+      };
+    } else {
+      gridContainerStyle = {
+        backgroundColor: backgroundColor,
+        gridBoardColumns: `repeat(${cols}, calc(100% / ${cols}))`,
+        gridBoardRows: `repeat(${rows}, calc(${currentWidth}px / ${cols}))`,
+      };
+    }
+    return gridContainerStyle;
+  };
+  
+export  const setGridContainerClass = (topsterType: string): string => {
+    return topsterType === "top42" ? "grid-container top42" : "grid-container";
+};
