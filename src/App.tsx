ae140af9147dccb8915b01d292ebd76fc256e03b
hojooneum <bldolphin96@gmail.com>
2022-11-27 15:33:42 +0900

    Refactor: provide Topster model by Context

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,14 @@ import { Helmet } from "react-helmet";
 import Main from "./components/mainComponents/Main";
 import Header from "./components/mainComponents/Header";
 import Footer from "./components/mainComponents/Footer";
+import { Topster } from "./models/Topster";
+import { createContext } from "react";
+
+export const TopsterContext = createContext<Topster>(new Topster());
 
 const App = (): JSX.Element => {
+  // existing topsteer will be fetched and provided by value for Provider on useEffect function
+
   return (
     <>
       <Helmet>
@@ -17,9 +23,11 @@ const App = (): JSX.Element => {
         <script src="https://cdn.jsdelivr.net/npm/uikit@3.14.1/dist/js/uikit.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/uikit@3.14.1/dist/js/uikit-icons.min.js"></script>
       </Helmet>
-      <Header />
-      <Main />
-      <Footer />
+      <TopsterContext.Provider value={new Topster()}>
+        <Header />
+        <Main />
+        <Footer />
+      </TopsterContext.Provider>
     </>
   );
 };
