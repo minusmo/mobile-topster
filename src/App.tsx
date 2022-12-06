7cb0a767319bb21ab900232c80340d6dd3a3fcc5
hojooneum <bldolphin96@gmail.com>
2022-12-06 16:03:07 +0900

    Refactor: remove uikit CDN

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Helmet } from "react-helmet";
 import Main from "./components/mainComponents/Main";
 import Header from "./components/mainComponents/Header";
-import Footer from "./components/mainComponents/Footer";
+import Footer from "./components/mainComponents/Footer/Footer";
 import { Topster } from "./models/Topster";
 import { createContext } from "react";
 import { LocalPersistencyManager } from "./services/PersistencyManager";
@@ -22,12 +22,6 @@ const App = (): JSX.Element => {
         <meta charSet="utf-8" />
         <meta name="description" content="mobile topster" />
         <title>Mobile-Topster</title>
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/uikit@3.14.1/dist/css/uikit.min.css"
-        />
-        <script src="https://cdn.jsdelivr.net/npm/uikit@3.14.1/dist/js/uikit.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/uikit@3.14.1/dist/js/uikit-icons.min.js"></script>
       </Helmet>
       <TopsterContext.Provider value={topster}>
         <Header />
