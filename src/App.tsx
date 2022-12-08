439df0560b29ddf3f3c503ba20f00bdadb079c49
hojooneum <bldolphin96@gmail.com>
2022-12-09 01:40:38 +0900

    Refactor: provide theme through App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import Home from "./Pages/Home/Home";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  padding: "1"
+}
 
 const App = (): JSX.Element => {
   // existing topster will be fetched 
   // and provided by value for Provider on useEffect
   return (
+    <ThemeProvider theme={theme}>
       <Home />
+    </ThemeProvider>
   );
 };
 
