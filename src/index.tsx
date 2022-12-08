912bd40308a5702ade3403ce547692f3d20870cf
hojooneum <bldolphin96@gmail.com>
2022-12-08 14:29:47 +0900

    Fix: fix import errors

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
-import "./styles/index.css";
+import "./assets/styles/index.css";
 import App from "./App";
 
 const root = createRoot(document.getElementById("root")!);
