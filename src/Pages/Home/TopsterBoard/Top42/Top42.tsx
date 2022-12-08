7738e49db893c801fc2c5e2923523d320defcbab
hojooneum <bldolphin96@gmail.com>
2022-12-09 01:39:47 +0900

    Refactor: extract Container component

--- src/Pages/Home/TopsterBoard/Top42/Top42.tsx
+++ src/Pages/Home/TopsterBoard/Top42/Top42.tsx
@@ -1,6 +1,7 @@
 import * as _ from "lodash";
 import { Album } from "../../../../models/Album"
-import { Row } from "../Grid/Row/Row"
+import { Row } from "../Row/Row"
+import { Container } from "../../../../components/Container";
 
 type Top42Props = {
     albums: Array<Album>
@@ -10,13 +11,13 @@ export const Top42 = ({
     albums
 }: Top42Props): JSX.Element => {
     return (
-        <div id="top42">
+        <Container>
             <Row key={_.uniqueId()} row={0} items={albums.slice(0,5)}/>
             <Row key={_.uniqueId()} row={1} items={albums.slice(5,10)}/>
             <Row key={_.uniqueId()} row={2} items={albums.slice(10,16)}/>
             <Row key={_.uniqueId()} row={3} items={albums.slice(16,22)}/>
-            <Row key={_.uniqueId()} row={4} items={albums.slice(22,33)}/>
-            <Row key={_.uniqueId()} row={5} items={albums.slice(33,43)}/>
-        </div>
+            <Row key={_.uniqueId()} row={4} items={albums.slice(22,32)}/>
+            <Row key={_.uniqueId()} row={5} items={albums.slice(32,42)}/>
+        </Container>
     )
 }
