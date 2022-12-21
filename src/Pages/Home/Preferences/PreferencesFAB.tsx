025edee95aa9ed9e50e6a8c08fc10a69a7a38eb7
hojooneum <bldolphin96@gmail.com>
2022-12-21 23:57:19 +0900

    refactor: refactor showPreferences function

--- src/Pages/Home/Preferences/PreferencesFAB.tsx
+++ src/Pages/Home/Preferences/PreferencesFAB.tsx
@@ -2,15 +2,15 @@ import { AdjustmentsVerticalIcon, XCircleIcon } from "@heroicons/react/24/outlin
 import { useState } from "react";
 
 type IPreferencesFAB = {
-  setShowPreferences: (showPreference: boolean) => void;
+  showPreferences: () => void;
 }
 
 export const PreferencesFAB = ({
-  setShowPreferences,
+  showPreferences,
 }: IPreferencesFAB) => {
   const [toggled, setToggled] = useState(false);
   return (
-    <button className="" onClick={() => { setToggled(!toggled); setShowPreferences(!toggled);}}>
+    <button onClick={() => { setToggled(!toggled); showPreferences();}}>
       {
         toggled ?
         <XCircleIcon />
