bffaab02c32e65844a2806e8683f588b0158efff
hojooneum <bldolphin96@gmail.com>
2022-12-23 23:47:30 +0900

    refactor: change dialog component to mui component

--- src/Pages/Home/Preferences/Preferences.tsx
+++ src/Pages/Home/Preferences/Preferences.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useRef } from "react";
+import { useContext, useRef } from "react";
 import { observer } from "mobx-react-lite";
 import styled from "styled-components";
 import { TopsterContext } from "../../../contexts/TopsterContext";
@@ -9,21 +9,17 @@ import { SubmitButton } from "../../../components/SubmitButton";
 import { Selection } from "../../../components/Selection";
 import { PreferencesFAB } from "./PreferencesFAB";
 import { action } from "mobx";
-import { TopsterType } from "../../../models/Topster";
+import { TopsterType } from "../../../data/models/Topster";
+import { Button, ButtonProps, DialogActions, DialogContent, DialogTitle } from "@mui/material";
 
 type IPreferences = {
   showAlbumTitles: boolean;
   setShowAlbumTitle: (showAlbumTitles: boolean) => void;
 };
 
-const PreferenceDialog = styled.dialog`
-  padding: 1rem;
-  border-radius: 5%;
-`;
-
-const PreferenceForm = styled.form`
-
-`;
+const CancelButton = (props: ButtonProps) => (
+  <Button {...props}></Button>
+)
 
 const Preferences = observer(({
   showAlbumTitles,
@@ -34,10 +30,11 @@ const Preferences = observer(({
   
   return (
     <>
-      <PreferencesFAB togglePreferences={() => {togglePreferences(PDialog.current)}}/>
-      <PreferenceDialog ref={PDialog} id={"preferences"}>
-        <PreferenceForm method="dialog">
-          <Toggle label={"Titles"} value={showAlbumTitles} ontoggle={setShowAlbumTitle} />
+      <PreferencesFAB onClick={() => {togglePreferences(PDialog.current)}}/>
+      <Dialog open={opened} onClose={hideDialog}>
+        <DialogTitle>Preferences</DialogTitle>
+        <DialogContent>
+          <Toggle label={"Titles"} value={showAlbumTitles} onChange={setShowAlbumTitle} />
           <Input label={"Background"} value={topster.backgroundColor} onchange={action((val: string) => {topster.backgroundColor = val;})}/>
           <Selection valueLabel={"Row"} value={topster.rows} onSelection={action((val: number) => {topster.rows = val;})} />
           <Selection valueLabel={"Col"} value={topster.cols} onSelection={action((val: number) => {topster.cols = val;})} />
@@ -45,9 +42,12 @@ const Preferences = observer(({
           <TextButton label={"SetTop42"} onClick={action(() => {topster.type = TopsterType.Top42})} />
           <TextButton label={"ClearCache"} onClick={() => {window.localStorage.clear()}} />
           <Toggle label={"Border"} value={topster.borderRoundness} ontoggle={action(() => {topster.borderRoundness = !topster.borderRoundness;})}/>
+        </DialogContent>
+        <DialogActions>
           <SubmitButton>{"Confirm"}</SubmitButton>
-        </PreferenceForm>
-      </PreferenceDialog>
+          <CancelButton>{"Cancel"}</CancelButton>
+        </DialogActions>
+      </Dialog>
     </>
   );
 });
