6a9e51706d488b8948477106850b32e82a8f8bef
hojooneum <bldolphin96@gmail.com>
2022-12-23 14:24:33 +0900

    refactor: refactor IconButton to styled component

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -1,20 +1,11 @@
-type IIconButton = {
-    onClick: () => void;
-    children: JSX.Element;
-};
-  
-export const IconButton = ({
-  onClick,
-  children
-}: IIconButton): JSX.Element => {
-  return (
-    <button
-      className=""
-      type="button"
-      onClick={() => onClick()}
-    >
-      {children}
-    </button>
-  );
-};
-  
+import styled from "styled-components";
+
+export const IconButton = styled.button`
+  width: 10vw;
+  height: 10vw;
+  border-radius: 50%;
+  border: none;
+  box-shadow: grey 0px 0px 5px;
+
+  cursor: pointer;
+`;
