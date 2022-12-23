b57086410bace425956d828b7c566378f89db0a5
hojooneum <bldolphin96@gmail.com>
2022-12-23 23:47:50 +0900

    refactor: change FAB component to mui component

--- src/Pages/Home/Preferences/PreferencesFAB.tsx
+++ src/Pages/Home/Preferences/PreferencesFAB.tsx
@@ -1,27 +1,22 @@
 import { AdjustmentsHorizontalIcon } from "@heroicons/react/24/outline";
 import { IconButton } from '../../../components/IconButton';
+import { Fab, FabProps } from "@mui/material";
+import TuneIcon from '@mui/icons-material/Tune';
 import styled from "styled-components";
 
 type IPreferencesFAB = {
   togglePreferences: () => void;
 }
 
-const RightFloatFAB = styled(IconButton)`
+export const PreferencesFAB = styled((props: FabProps) => {
+  const { onClick } = props;
+  return (
+    <Fab onClick={onClick} size={"medium"}>
+      <TuneIcon />
+    </Fab>
+  );
+})`
   position: fixed;
   bottom: 20vh;
-  right: 5px;
-
-  &:hover {
-    background-color: lime;
-  }
+  right: 10px;
 `;
-
-export const PreferencesFAB = ({
-  togglePreferences,
-}: IPreferencesFAB) => {
-  return (
-    <RightFloatFAB onClick={() => {togglePreferences();}}>
-        <AdjustmentsHorizontalIcon width={'5vw'} height={'5vw'}/>
-    </RightFloatFAB>
-  );
-};
