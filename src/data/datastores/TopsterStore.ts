2bcf8a55add013053fb9ede710e8602f4215a219
hojooneum <bldolphin96@gmail.com>
2022-12-24 23:41:05 +0900

    refactor: add TopsterStore and TopstertStoreContext

--- src/data/datastores/TopsterStore.ts
+++ src/data/datastores/TopsterStore.ts
@@ -1,3 +1,51 @@
+import { makeAutoObservable, reaction } from 'mobx';
+import { LocalPersistency } from "../../services/Persistency";
+import { Topster } from "../models/Topster";
+
+type SelectedPosition = {
+    row: number;
+    col: number;
+}
+
 export default class TopsterStore {
-    
+    topster: Topster | undefined;
+    selectedPosition: SelectedPosition = {
+        row: 0,
+        col: 0,
+    };
+
+    constructor() {
+        makeAutoObservable(this);
+        this.initializeTopster();
+    }
+
+    private saveLocally(): void {
+        if (!this.topster) return;
+        try {
+            LocalPersistency.save('topster', this.topster.toString());
+        }
+        catch(e) {
+            throw new Error('local persistency saving failed');
+        }
+    }
+
+    private initializeTopster() {
+        const savedTopster = LocalPersistency.retrieve('topster');
+        const topster = new Topster();
+        if (savedTopster) {
+            topster.fromJson(JSON.parse(savedTopster));
+        }
+        this.topster = topster;
+    }
+
+    private effect = reaction(
+        () => (this.topster),
+        topster => {
+            this.saveLocally();
+        }
+    )
+
+    public getSelectedIdx(): number {
+        return this.selectedPosition.row + this.selectedPosition.col;
+    }
 }
