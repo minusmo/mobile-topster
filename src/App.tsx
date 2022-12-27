87177469be99b1631fa6793469fae113219f24a3
hojooneum <bldolphin96@gmail.com>
2022-12-27 23:41:07 +0900

    feat: provide QueryClient to Home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import Home from "./Pages/Home/Home";
 import { ThemeProvider } from "styled-components";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const theme = {
   padding: "1"
@@ -9,9 +12,11 @@ const App = (): JSX.Element => {
   // existing topster will be fetched 
   // and provided by value for Provider on useEffect
   return (
-    <ThemeProvider theme={theme}>
-      <Home />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <Home />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
 
