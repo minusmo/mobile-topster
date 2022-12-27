d3ffa854b60c2f457c4a66ab55259fde9154d84f
hojooneum <bldolphin96@gmail.com>
2022-12-28 01:37:38 +0900

    feat: add SwipeableDrawer component to SearchPanel

--- src/Pages/Home/SearchPanel/SearchPanel.tsx
+++ src/Pages/Home/SearchPanel/SearchPanel.tsx
@@ -1,17 +1,21 @@
 import React, { useState, useContext } from "react";
 import { useQuery } from "@tanstack/react-query";
+import { SwipeableDrawer } from "@mui/material";
+import Skeleton from "@mui/material/Skeleton";
+import { action } from "mobx";
 import { TopsterStoreContext } from "../../../contexts/TopsterStoreContext";
-import TopsterStore from "../../../data/datastores/TopsterStore";
-import { Topster } from "../../../data/models/Topster";
+import { TopsterStore } from "../../../data/datastores/TopsterStore";
 import { Album } from "../../../data/models/Album";
 import { queryAlbumsToSpotify } from "./queryAlbumsToSpotify";
-import { SpotifyAlbumData } from "./types";
 import SearchForm from "./SearchForm";
 import { ImgList } from "./ImgList";
+import { Topster } from "../../../data/models/Topster";
+import { observer } from "mobx-react-lite";
+import { Puller } from "./Puller";
 
-const SearchPanel = (): JSX.Element => {
-  const topsterStore = useContext(TopsterStoreContext);
-  const topster = topsterStore.topster;
+const SearchPanel = observer((): JSX.Element => {
+  const topsterStore: TopsterStore = useContext(TopsterStoreContext);
+  const topster: Topster = topsterStore.topster;
   const [query, setQuery] = useState("");
   const [country, setCountry] = useState("us");
   const [searchResult, setSearchResult] = useState<Array<Album>>([]);
@@ -20,28 +24,40 @@ const SearchPanel = (): JSX.Element => {
     queryFn: () => {queryAlbumsToSpotify(query, country)},
     enabled: false,
   });
-
+  
   return (
-    <div>
+    <SwipeableDrawer
+      anchor={'left'}
+      open={topsterStore.selectedIdx === -1 ? false : true}
+      onClose={action(() => topsterStore.selectedIdx = -1)}
+      onOpen={() => {}}
+    >
+      <Puller />
       <SearchForm
         setCountry={setCountry}
         setQuery={setQuery}
         onSubmission={() => {refetch()}}
       />
-      <ImgList 
-        albums={searchResult}
-        addAlbum={addAlbum}
-      />
-    </div>
+      {isLoading ?
+        <Skeleton
+          variant={'rectangular'}
+        />
+        :
+        <ImgList 
+          albums={searchResult}
+          addAlbum={addAlbum}
+        />
+      }
+    </SwipeableDrawer>
   );
 
   function addAlbum(newAlbum: Album): void {
       topster.replaceAlbumAt(
-        topsterStore.getSelectedIdx(),
+        topsterStore.selectedIdx,
         newAlbum
       );
   }
-};
+});
 
 export default SearchPanel;
 
