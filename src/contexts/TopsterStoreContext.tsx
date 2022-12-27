bd2ab0d10e707d79110fba1b725b7675b1100636
hojooneum <bldolphin96@gmail.com>
2022-12-28 01:34:08 +0900

    refactor: change TopsterStore to module not default module

--- src/contexts/TopsterStoreContext.tsx
+++ src/contexts/TopsterStoreContext.tsx
@@ -1,5 +1,5 @@
 import { createContext } from "react";
-import TopsterStore from "../data/datastores/TopsterStore";
+import { TopsterStore } from "../data/datastores/TopsterStore";
 
 export const topsterStore: TopsterStore = new TopsterStore();
 export const TopsterStoreContext = createContext<TopsterStore>(topsterStore);
