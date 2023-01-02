57277fcd0ed77bd031b56cc27164cee81dd21b88
hojooneum <bldolphin96@gmail.com>
2023-01-02 20:28:47 +0900

    feat: fix subheader in ImgList style

--- src/Pages/Home/SearchPanel/ImgList.tsx
+++ src/Pages/Home/SearchPanel/ImgList.tsx
@@ -1,6 +1,8 @@
 import { IconButton, ImageList, ImageListItem, ImageListItemBar, ListSubheader } from "@mui/material";
 import { Album } from "../../../data/models/Album";
 import AddIcon from '@mui/icons-material/Add';
+import { Box } from "@mui/system";
+import { action } from "mobx";
 
 interface IImgList {
     albums: Album[];
@@ -12,28 +14,32 @@ export const ImgList = ({
     addAlbum
 }: IImgList): JSX.Element => {
     return (
-        <ImageList>
-            <ImageListItem key={'Subheader'}>
-                <ListSubheader component={'div'}>All matches found</ListSubheader>
-            </ImageListItem>
-            {albums.map((album: Album) => (
-                <ImageListItem key={album.art}>
-                    <img
-                        src={album.art}
-                        alt={album.info()}
-                        loading={'lazy'}
-                    />
-                    <ImageListItemBar
-                        title={album.title}
-                        subtitle={album.artist}
-                        actionIcon={
-                            <IconButton onClick={() => {addAlbum(album)}}>
-                                <AddIcon />
-                            </IconButton>
-                        }
-                    />
+        <Box sx={{
+            padding: '10px 0',
+        }}>
+            <ImageList cols={3}>
+                <ImageListItem key={'Subheader'} cols={3}>
+                    <ListSubheader component={'div'}>All matches found</ListSubheader>
                 </ImageListItem>
-            ))}
-        </ImageList>
+                {albums.map((album: Album) => (
+                    <ImageListItem key={album.art}>
+                        <img
+                            src={album.art}
+                            alt={album.info()}
+                            loading={'lazy'}
+                        />
+                        <ImageListItemBar
+                            title={album.title}
+                            subtitle={album.artist}
+                            actionIcon={
+                                <IconButton onClick={action('addAlbum', () => {addAlbum(album)})}>
+                                    <AddIcon />
+                                </IconButton>
+                            }
+                        />
+                    </ImageListItem>
+                ))}
+            </ImageList>
+        </Box>
     )
 }
