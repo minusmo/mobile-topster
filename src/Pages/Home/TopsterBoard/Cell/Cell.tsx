bc86b0ae4f678841f8b7664568046113a7cbfafc
hojooneum <bldolphin96@gmail.com>
2023-01-02 20:25:56 +0900

    refactor: add action names to actions

--- src/Pages/Home/TopsterBoard/Cell/Cell.tsx
+++ src/Pages/Home/TopsterBoard/Cell/Cell.tsx
@@ -3,6 +3,7 @@ import { Album } from "../../../../data/models/Album";
 import styled from "styled-components";
 import { TopsterStoreContext } from "../../../../contexts/TopsterStoreContext";
 import { action } from "mobx";
+import { observer } from "mobx-react-lite";
 
 
 type ICell = {
@@ -11,7 +12,7 @@ type ICell = {
     item: Album;
 }
 
-export const Cell = ({
+export const Cell = observer(({
     rowItemsPassed,
     colItemsPassed,
     item,
@@ -22,7 +23,7 @@ export const Cell = ({
     const ImgContainer = styled.div`
         width: 100%;
         aspect-ratio: 1/1;
-        padding: ${topster.gridGap}px;
+        padding: 0 ${topster.gridGap}px;
     `
     
     const Img = styled.img`
@@ -36,10 +37,12 @@ export const Cell = ({
             <Img 
                 src={item.art} 
                 alt={`${item.title}-${item.artist}`} 
-                onClick={action(() => {
+                onClick={action(
+                    'setSelectedIdx',
+                    () => {
                     topsterStore.selectedIdx = rowItemsPassed + colItemsPassed;
                 })}
             />
         </ImgContainer>
     );
-}
+})
