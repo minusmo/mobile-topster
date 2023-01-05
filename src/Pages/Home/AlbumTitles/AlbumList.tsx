04dad49450aa69553e709a48bd2317f8c18a082b
hojooneum <bldolphin96@gmail.com>
2023-01-06 03:51:21 +0900

    refactor: refactor Album title list components

---
+++ src/Pages/Home/AlbumTitles/AlbumList.tsx
@@ -0,0 +1,21 @@
+import { List } from "@mui/material";
+import { Album } from "../../../data/models/Album";
+import AlbumTitle from "./AlbumTitle/AlbumTitle";
+
+interface IAlbumList {
+  albums: Album[];
+  start: number;
+  end: number;
+}
+
+export function AlbumList({
+  albums, 
+  start = 0, 
+  end = albums.length
+}: IAlbumList): JSX.Element {
+  return (
+    <List>
+      {albums.slice(start, end).map((album, idx) => <AlbumTitle key={idx} album={album} />)}
+    </List>
+  );
+}
