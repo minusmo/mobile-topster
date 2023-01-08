b7098afe6afc8b8faaaef3e090ef218cebe05d60
hojooneum <bldolphin96@gmail.com>
2023-01-08 19:00:34 +0900

    refactor: reorganize page specific components for SearchPanel

--- src/Pages/Home/SearchPanel/ImgList/ImgList.tsx
+++ src/Pages/Home/SearchPanel/ImgList/ImgList.tsx
@@ -1,5 +1,5 @@
 import { IconButton, ImageList, ImageListItem, ImageListItemBar, ListSubheader } from "@mui/material";
-import { Album } from "../../../data/models/Album";
+import { Album } from "../../../../data/models/Album";
 import AddIcon from '@mui/icons-material/Add';
 import { Box } from "@mui/system";
 import { action } from "mobx";
