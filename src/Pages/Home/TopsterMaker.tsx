bee11cf5ff09f297a02c7e825e79327942563e92
hojooneum <bldolphin96@gmail.com>
2023-01-12 11:26:54 +0900

    refactor: pass reference for captured area to TopsterBoard

--- src/Pages/Home/TopsterMaker.tsx
+++ src/Pages/Home/TopsterMaker.tsx
@@ -1,37 +1,35 @@
-import _ from "lodash";
-import * as htmlToImage from "html-to-image";
 import { useState, useEffect, useRef } from "react";
 import { observer } from "mobx-react-lite";
-import { saveAs } from "file-saver";
 import SearchPanel from "./SearchPanel/SearchPanel";
 import TopsterBoard from "./TopsterBoard/TopsterBoard";
 import Preferences from "./Preferences/Preferences";
-import SaveButton from "./SaveButton";
+import SaveButton from "./SaveButton/SaveButton";
 import HelpMessages from "./HelpMessages/HelpMessages";
 import { LocalPersistency } from "../../services/Persistency";
+import useImgSave from "./SaveButton/useImgSave";
 
 const savedTitlesState = JSON.parse(LocalPersistency.retrieve("showAlbumTitles")) ? true : false;
 
 const TopsterMaker = observer((): JSX.Element => {
   const [showAlbumTitles, setShowAlbumTitle] = useState(savedTitlesState);
-  const [processingSave, setProcessingSave] = useState(false);
-  const screenshotArea = useRef<HTMLElement | null>(null);
+  // const [processingSave, setProcessingSave] = useState(false);
+  const capturedArea = useRef<HTMLElement | null>(null);
+  const {updateCapturedArea, captureArea} = useImgSave(capturedArea.current);
 
   useEffect(() => {
-    screenshotArea.current = document.querySelector("#screenshot-area") as HTMLElement;
+    updateCapturedArea(capturedArea.current!);
 
     return () => {
       LocalPersistency.save("showAlbumTitles", JSON.stringify(showAlbumTitles));
     };
   }, []);
 
-  const {handleSave} = useImgSave();
-
   return (
     <div id="topster-maker">
       {/* 탑스터 */}
-      <TopsterBoard 
-        showAlbumTitles={showAlbumTitles} 
+      <TopsterBoard
+        showAlbumTitles={showAlbumTitles}
+        capturedAreaRef={capturedArea}
       />
       {/* 검색창  */}
       <SearchPanel />
@@ -43,7 +41,7 @@ const TopsterMaker = observer((): JSX.Element => {
       {/* 도움말 */}
       <HelpMessages />
       {/* 저장 버튼 */}
-      <SaveButton imgSaveHandler={handleSave} />
+      <SaveButton imgSaveHandler={captureArea} />
     </div>
   );
 });
