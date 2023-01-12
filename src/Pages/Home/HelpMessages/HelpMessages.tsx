cae5a2a1352afb85c8736355663e79a82eafc4e3
hojooneum <bldolphin96@gmail.com>
2023-01-12 11:28:39 +0900

    feat: add additional help text to HelpMessages

--- src/Pages/Home/HelpMessages/HelpMessages.tsx
+++ src/Pages/Home/HelpMessages/HelpMessages.tsx
@@ -11,13 +11,18 @@ const HelpMessages = () => {
         <DialogTitle>Some Helps</DialogTitle>
         <DialogContent>
           <Typography variant="subtitle1">
-            이미지를 클릭하면 새로운 앨범을 검색하고 추가할 수 있습니다.
+            빈 앨범을 클릭하면 새로운 앨범을 검색하고 추가할 수 있습니다.
           </Typography>
           <Typography variant="subtitle1">
-            Chrome 브라우저에서 정상 작동합니다.
+            앨범을 더블 클릭하면 앨범을 지울 수 있습니다.
           </Typography>
           <Typography variant="subtitle1">
-            문의/버그신고: bldolphin96@gmail.com
+            Chrome(Mac/Windows/Android) 브라우저에서 정상 작동합니다.
+            <br/>
+            Safari는 이미지 저장에 문제가 발생할 수 있습니다.
+          </Typography>
+          <Typography variant="subtitle1">
+            문의/버그 신고: bldolphin96@gmail.com
           </Typography>
         </DialogContent>
         <DialogActions>
